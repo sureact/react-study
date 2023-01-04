a76b235fbdc46998173df5a6802b732f5461c8aa
milk717 <robolindasoo@gmail.com>
2023-01-04 18:30:01 +0900

    10장: 일정 관리 웹 어플리케이션 만들기

---
+++ src/component/TodoListItem.js
@@ -0,0 +1,20 @@
+import '../style/TodoListItem.scss';
+import {MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline} from "react-icons/md";
+import cn from 'classnames';
+
+const TodoListItem = ({todo, onRemove, onToggle}) =>{
+    const {id, text, checked} = todo;
+    return(
+        <div className="TodoListItem">
+            <div className={cn('checkbox',{checked})}  onClick={()=>onToggle(id)}>
+                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                <div className="text">{text}</div>
+            </div>
+            <div className="remove" onClick={()=>onRemove(id)}>
+                <MdRemoveCircleOutline/>
+            </div>
+        </div>
+    )
+}
+
+export default TodoListItem;
