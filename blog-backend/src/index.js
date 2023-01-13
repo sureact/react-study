e8dbc5bafc5b2182a33f76923937f5aa5363ddc6
milk717 <robolindasoo@gmail.com>
2023-01-13 22:37:06 +0900

    21장: koa-router 분리

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,24 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', ctx => {
-    ctx.body = '홈';
-});
-
-//url 파라미터 조회하기
-router.get('/about/:name?', ctx => {
-    const {name} = ctx.params;
-    ctx.body = name ? `${name}의 소개`: '소개';
-});
-
-//url 쿼리스트링 조회하기
-router.get('/posts',ctx=>{
-    const {id} = ctx.query;
-    ctx.body = id ? `포스트 #${id}`: '아이디가 존재하지 않습니다.';
-})
+router.use(api.routes());
 
 // app 인스턴스에 라우터 적용하기
 app.use(router.routes()).use(router.allowedMethods());
