f338a713ad00bef11023b421b58d3d4cb526faeb
milk717 <robolindasoo@gmail.com>
2023-01-13 23:42:21 +0900

    21장: http 메서드 동작 결과 리스트에 저장

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -8,9 +9,11 @@ const router = new Router();
 
 router.use('/api',api.routes());
 
+// 라우터를 적용하기 전에 bodyParser를 먼저 적용해야함.
+app.use(bodyParser());
+
 // app 인스턴스에 라우터 적용하기
 app.use(router.routes()).use(router.allowedMethods());
-
 app.listen(4000, ()=>{
     console.log('listening to port 4000');
 })
