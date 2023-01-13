63ef134c600b18f9e8276ae6eedce094ab7be3e2
milk717 <robolindasoo@gmail.com>
2023-01-13 22:56:48 +0900

    21장: koa-router, http 메소드 사용하기

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -6,7 +6,7 @@ const api = require('./api');
 const app = new Koa();
 const router = new Router();
 
-router.use(api.routes());
+router.use('/api',api.routes());
 
 // app 인스턴스에 라우터 적용하기
 app.use(router.routes()).use(router.allowedMethods());
