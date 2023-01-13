521df139f62ee10ce3e63501a6e7f8865b22c14b
milk717 <robolindasoo@gmail.com>
2023-01-13 22:30:09 +0900

    21장: koa-router 기본 사용법

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -4,24 +4,24 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-app.use(async (ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
-    if (ctx.query.authorized !== '1'){
-        ctx.status = 401;
-        return;
-    }
-    await next();
-    console.log('End')
-})
-app.use((ctx, next) => {
-    console.log(2)
-    next();
-})
-app.use(ctx=>{
-    console.log(3)
-    ctx.body = 'hello world!!';
+router.get('/', ctx => {
+    ctx.body = '홈';
 });
+
+//url 파라미터 조회하기
+router.get('/about/:name?', ctx => {
+    const {name} = ctx.params;
+    ctx.body = name ? `${name}의 소개`: '소개';
+});
+
+//url 쿼리스트링 조회하기
+router.get('/posts',ctx=>{
+    const {id} = ctx.query;
+    ctx.body = id ? `포스트 #${id}`: '아이디가 존재하지 않습니다.';
+})
+
+// app 인스턴스에 라우터 적용하기
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, ()=>{
     console.log('listening to port 4000');
